e6f48eb72354a529150206db108f5536a29508c8
deyciandreacamargoramos5-creator <dcamargor@unsa.edu.pe>
2025-11-24 17:17:51 -0500

    Ejercicio 3

--- Ejercicio3.js
+++ Ejercicio3.js
@@ -1,9 +1,12 @@
 function ErrorProvocado(){
-try{
-    console.log(c);
-}catch (error){
-    console.log("Fallo la ejecución");
-}finally{
-    console.log("Siempre se ejecuta al final");
+    try{
+        let edad= -5;
+        if(edad < 0){
+            throw new Error("La edad no ser un número negativo.");
+        }
+        console.log("L edad es valida");
+    }catch (e){
+        console.log("Error de validación");
+        console.log(e.message);
     }
 }
